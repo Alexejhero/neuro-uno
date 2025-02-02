1280c3a62a29ddc2ba6406913040da4e3a29a81f
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2025-02-02 17:55:25 +0200

    Add starter plugin

---
+++ NeuroUno/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace NeuroUno;
+
+[BepInAutoPlugin]
+public sealed partial class Plugin : BasePlugin
+{
+    public override void Load()
+    {
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Id);
+    }
+}
