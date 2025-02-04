931688890382433c0cf3747861c7fe5fa9bbe0d3
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2025-02-04 07:41:09 +0200

    Starting point

---
+++ NeuroUno/GlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿extern alias JetBrains;
+global using MeansImplicitUse = JetBrains::JetBrains.Annotations.MeansImplicitUseAttribute;
+global using UsedImplicitly = JetBrains::JetBrains.Annotations.UsedImplicitlyAttribute;
--- NeuroUno/Plugin.cs
+++ NeuroUno/Plugin.cs
@@ -2,14 +2,28 @@
 using BepInEx;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
+using Il2CppInterop.Runtime;
+using Il2CppInterop.Runtime.Injection;
+using NeuroSdk;
+using UnityEngine;
+using CollectionExtensions = BepInEx.Unity.IL2CPP.Utils.Collections.CollectionExtensions;
 
 namespace NeuroUno;
 
-[BepInAutoPlugin]
+[BepInAutoPlugin, UsedImplicitly]
 public sealed partial class Plugin : BasePlugin
 {
     public override void Load()
     {
         Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Id);
+        AddComponent<Test>();
+    }
+}
+
+public class Test : MonoBehaviour
+{
+    public void Start()
+    {
+        NeuroSdkSetup.Initialize("Test");
     }
 }
